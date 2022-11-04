b5c351d507afc12ab76b5325095bb621d3fee348
Skeletonxf <skeletonxf@gmail.com>
2022-11-04 23:16:30 +0000

    Make pathing logic a little less hacky

--- ui/src/main/kotlin/io/github/skeletonxf/Bridge.kt
+++ ui/src/main/kotlin/io/github/skeletonxf/Bridge.kt
@@ -3,13 +3,18 @@ package io.github.skeletonxf
 import io.github.skeletonxf.bindings.JavaTest
 import io.github.skeletonxf.bindings.bindings_h
 import java.nio.file.Path
-import java.nio.file.Paths
 
 class Bridge {
     init {
-        // There has to be a nicer way to work out our path than hacking it with strings
-        val projectRoot = Paths.get("").toAbsolutePath().toString().removeSuffix("/ui")
-        System.load("$projectRoot/target/debug/libhnefatafl.so")
+        val currentDirectory = Path.of("").toAbsolutePath()
+        val projectRoot = if (currentDirectory.endsWith("ui")) {
+            currentDirectory.parent
+        } else {
+            currentDirectory
+        }
+        // TODO: Need to look for correct library on Windows / MacOS as well and also look for release build
+        val library = projectRoot.resolve(Path.of("target/debug/libhnefatafl.so"))
+        System.load(library.toString())
     }
 
     fun helloWorld() {
