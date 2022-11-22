d187673b49bc7d5865dc1bedc2f5cf00a03d84ed
Skeletonxf <skeletonxf@gmail.com>
2022-11-22 22:34:51 +0000

    Get jextract being called at build time from gradle, stop checking Java glue code into repo

--- ui/build.gradle.kts
+++ ui/build.gradle.kts
@@ -1,4 +1,5 @@
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import java.util.Properties
 
 plugins {
     kotlin("jvm")
@@ -27,6 +28,32 @@ configure<SourceSetContainer> {
 
 val jvmVersion = 19
 
+val properties = Properties().apply {
+    load(rootProject.file("local.properties").reader())
+}
+val jextractPath = properties["jextract-path"]
+
+val generateBindings = tasks.register("generateBindings", Exec::class) {
+    workingDir = File("${project.projectDir}")
+    // The ui folder is inside the root folder, and the root folder is from where we need to reach
+    // the rust code
+    val sourceRoot = project.projectDir.parent
+    if (jextractPath == null || jextractPath == "") {
+        throw IllegalArgumentException("path to JExtract executable must be specified in local.properties")
+    }
+    commandLine = listOf(
+        jextractPath as String,
+        "--source",
+        "-t",
+        "io.github.skeletonxf.bindings",
+        "--output",
+        "src/main/kotlin/",
+        "$sourceRoot/bindings.h",
+        "-l",
+        "$sourceRoot/target/debug/libhnefatafl.so", // FIXME: This won't work on Windows
+    ).also { println("Building bindings: ${it.joinToString(separator = " ")}") }
+}
+
 tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile::class).all {
     // 19 doesn't seem to be recognised yet?
     // kotlinOptions {
@@ -40,6 +67,7 @@ java {
 
 tasks.withType(JavaCompile::class).all {
     options.compilerArgs.add("--enable-preview")
+    dependsOn.add(generateBindings)
 }
 
 compose.desktop {
--- ui/src/main/kotlin/io/github/skeletonxf/Bridge.kt
+++ ui/src/main/kotlin/io/github/skeletonxf/Bridge.kt
@@ -1,24 +1,10 @@
 package io.github.skeletonxf
 
-import io.github.skeletonxf.bindings.JavaTest
 import io.github.skeletonxf.bindings.bindings_h
-import java.nio.file.Path
 
 class Bridge {
-    init {
-        val currentDirectory = Path.of("").toAbsolutePath()
-        val projectRoot = if (currentDirectory.endsWith("ui")) {
-            currentDirectory.parent
-        } else {
-            currentDirectory
-        }
-        // TODO: Need to look for correct library on Windows / MacOS as well and also look for release build
-        val library = projectRoot.resolve(Path.of("target/debug/libhnefatafl.so"))
-        System.load(library.toString())
-    }
-
     fun helloWorld() {
-        JavaTest.helloWorld()
+        println("Hello from Kotlin")
         bindings_h.hello_from_rust()
     }
 }
