6e5866a5bd6c65fe3fb96aed69b4de5ead189a7b
Skeletonxf <skeletonxf@gmail.com>
2022-11-23 22:23:02 +0000

    Start building out an opaque handle for the game state that is used over FFI

--- bindings.h
+++ bindings.h
@@ -1,1 +1,9 @@
+typedef struct GameStateHandle GameStateHandle;
+
+struct GameStateHandle *game_state_handle_new(void);
+
+void game_state_handle_destroy(struct GameStateHandle *handle);
+
+void game_state_handle_debug(struct GameStateHandle *handle);
+
 void hello_from_rust(void);
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,68 @@
+mod bot;
+mod piece;
+mod state;
+
+use state::GameState;
+
+#[derive(Debug)]
+pub struct GameStateHandle {
+    state: GameState,
+}
+
+impl GameStateHandle {
+    fn new() -> Self {
+        GameStateHandle {
+            state: GameState::default(),
+        }
+    }
+}
+
+#[no_mangle]
+pub extern fn game_state_handle_new() -> *mut GameStateHandle {
+    let boxed = Box::new(GameStateHandle::new());
+    // let the caller be responsible for managing this memory now
+    Box::into_raw(boxed)
+}
+
+#[no_mangle]
+pub extern fn game_state_handle_destroy(handle: *mut GameStateHandle) {
+    if handle.is_null() {
+        return;
+    }
+    std::mem::drop(unsafe {
+        Box::from_raw(handle)
+    });
+}
+
+#[no_mangle]
+pub extern fn game_state_handle_debug(handle: *mut GameStateHandle) {
+    if let Err(error) = with_handle(handle, |handle| {
+        println!("Game state handle:\n{:?}", handle);
+    }) {
+        eprint!("Error calling game_state_handle_debug: {:?}", error);
+    }
+}
+
+#[derive(Clone, Debug)]
+enum FFIError {
+    NullPointer,
+    Panic,
+}
+
+fn with_handle<F, R>(handle: *mut GameStateHandle, op: F) -> Result<R, FFIError>
+where
+    F: FnOnce(&mut GameStateHandle) -> R + std::panic::UnwindSafe,
+{
+    if handle.is_null() {
+        return Err(FFIError::NullPointer)
+    }
+    std::panic::catch_unwind(|| {
+        let handle = unsafe {
+            &mut *handle
+        };
+        op(handle)
+    }).map_err(|_| FFIError::Panic)
+}
 
 #[no_mangle]
 pub extern "C" fn hello_from_rust() {
