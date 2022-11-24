528bb27202da273498fcfd075f1930beb1b7025a
Skeletonxf <skeletonxf@gmail.com>
2022-11-24 18:43:46 +0000

    Make the FFI dereferencing a little safer
    Now only need destroy to be completely unsafe, normal methods using aliased handle shouldn't violate Rust's memory model

--- bindings.h
+++ bindings.h
@@ -1,9 +1,20 @@
 typedef struct GameStateHandle GameStateHandle;
 
+/**
+ * Creates a new GameStateHandle
+ */
 struct GameStateHandle *game_state_handle_new(void);
 
+/**
+ * Destroys the data owned by the pointer
+ * The caller is responsible for ensuring there are no aliased references elsewhere in the
+ * program
+ */
 void game_state_handle_destroy(struct GameStateHandle *handle);
 
-void game_state_handle_debug(struct GameStateHandle *handle);
+/**
+ * Prints the game state
+ */
+void game_state_handle_debug(const struct GameStateHandle *handle);
 
 void hello_from_rust(void);
--- src/lib.rs
+++ src/lib.rs
@@ -2,21 +2,24 @@ mod bot;
 mod piece;
 mod state;
 
+use std::sync::Mutex;
+
 use state::GameState;
 
 #[derive(Debug)]
 pub struct GameStateHandle {
-    state: GameState,
+    state: Mutex<GameState>,
 }
 
 impl GameStateHandle {
     fn new() -> Self {
         GameStateHandle {
-            state: GameState::default(),
+            state: Mutex::new(GameState::default()),
         }
     }
 }
 
+/// Creates a new GameStateHandle
 #[no_mangle]
 pub extern fn game_state_handle_new() -> *mut GameStateHandle {
     let boxed = Box::new(GameStateHandle::new());
@@ -24,18 +27,20 @@ pub extern fn game_state_handle_new() -> *mut GameStateHandle {
     Box::into_raw(boxed)
 }
 
+/// Destroys the data owned by the pointer
+/// The caller is responsible for ensuring there are no aliased references elsewhere in the
+/// program
 #[no_mangle]
-pub extern fn game_state_handle_destroy(handle: *mut GameStateHandle) {
+pub unsafe extern fn game_state_handle_destroy(handle: *mut GameStateHandle) {
     if handle.is_null() {
         return;
     }
-    std::mem::drop(unsafe {
-        Box::from_raw(handle)
-    });
+    std::mem::drop(Box::from_raw(handle));
 }
 
+/// Prints the game state
 #[no_mangle]
-pub extern fn game_state_handle_debug(handle: *mut GameStateHandle) {
+pub extern fn game_state_handle_debug(handle: *const GameStateHandle) {
     if let Err(error) = with_handle(handle, |handle| {
         println!("Game state handle:\n{:?}", handle);
     }) {
@@ -49,18 +54,33 @@ enum FFIError {
     Panic,
 }
 
-fn with_handle<F, R>(handle: *mut GameStateHandle, op: F) -> Result<R, FFIError>
+/// Takes an (optionally) aliased handle to the game state, unlocks the mutex and performs
+/// and operation with a non aliased mutable reference to the game state, returning the
+/// result of the operation or an error if there was a failure with the FFI.
+fn with_handle<F, R>(handle: *const GameStateHandle, op: F) -> Result<R, FFIError>
 where
-    F: FnOnce(&mut GameStateHandle) -> R + std::panic::UnwindSafe,
+    F: FnOnce(&mut GameState) -> R + std::panic::UnwindSafe,
 {
     if handle.is_null() {
         return Err(FFIError::NullPointer)
     }
     std::panic::catch_unwind(|| {
+        // SAFETY: We only give out valid pointers, and are trusting that the Kotlin code
+        // does not invalidate them.
         let handle = unsafe {
-            &mut *handle
+            &*handle
         };
-        op(handle)
+        // Since the Kotlin side can freely alias as much as it likes, we put the aliased handle
+        // around a Mutex so we can ensure no aliasing for the actual game state
+        let mut guard = match handle.state.lock() {
+            Ok(guard) => guard,
+            Err(poison_error) => {
+                eprintln!("Poisoned mutex: {}", poison_error);
+                poison_error.into_inner()
+            },
+        };
+        op(&mut guard)
+        // drop mutex guard
     }).map_err(|_| FFIError::Panic)
 }
 
