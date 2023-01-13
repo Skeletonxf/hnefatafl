f8524f437da384e3b04599f7bbc29ab381242ed0
Skeletonxf <skeletonxf@gmail.com>
2023-01-13 21:02:57 +0000

    Add window icon

--- ui/src/main/kotlin/main.kt
+++ ui/src/main/kotlin/main.kt
@@ -5,8 +5,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.awt.ComposeWindow
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.toAwtImage
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
@@ -16,6 +23,7 @@ import io.github.skeletonxf.HnefataflMaterialTheme
 import io.github.skeletonxf.PreviewSurface
 import io.github.skeletonxf.board.Board
 import io.github.skeletonxf.board.Tile
+import java.awt.SystemColor.window
 
 @Composable
 @Preview
@@ -53,12 +61,24 @@ private fun ContentPreview() = PreviewSurface {
 fun main() {
     val state = GameStateHandle()
     val board = state.board()
+
     application {
         Window(
             onCloseRequest = ::exitApplication,
             title = "Hnefatafl",
         ) {
+            IconSideEffect(window)
             App(board)
         }
+    }
+}
+
+@Composable
+private fun IconSideEffect(composeWindow: ComposeWindow) {
+    // Workaround for https://github.com/JetBrains/compose-jb/issues/1838
+    val icon = painterResource("images/icon.svg")
+    val density = LocalDensity.current
+    SideEffect {
+        composeWindow.iconImage = icon.toAwtImage(density, LayoutDirection.Ltr, Size(128F, 128F))
     }
 }
