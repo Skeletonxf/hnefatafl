15a65e0662f3027bf55f5a9ab38729b84cbda8c3
Skeletonxf <skeletonxf@gmail.com>
2023-06-10 11:53:47 +0100

    Stop build script causing issues when it can't parse the code

--- build.rs
+++ build.rs
@@ -6,10 +6,14 @@ use std::env;
 fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
 
-    cbindgen::Builder::new()
-      .with_crate(crate_dir)
-      .with_language(Language::C)
-      .generate()
-      .expect("Unable to generate bindings")
-      .write_to_file("bindings.h");
+    match cbindgen::Builder::new()
+        .with_crate(crate_dir)
+        .with_language(Language::C)
+        .generate()
+    {
+        Ok(bindings) => {
+            bindings.write_to_file("bindings.h");
+        }
+        Err(error) => eprintln!("Error: {}", error),
+    }
 }
