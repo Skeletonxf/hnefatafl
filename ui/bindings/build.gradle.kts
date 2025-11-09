94b73f912143e5b8c325f64a8bdd5cc2d8024cc7
Skeletonxf <skeletonxf@gmail.com>
2025-11-09 19:13:20 +0000

    Migrate bindings module to KMP to get Android target builds complete

--- ui/bindings/build.gradle.kts
+++ ui/bindings/build.gradle.kts
@@ -1,16 +1,35 @@
 import gobley.gradle.GobleyHost
 import gobley.gradle.cargo.dsl.jvm
 import gobley.gradle.Variant
+import gobley.gradle.cargo.dsl.android
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-    alias(libs.plugins.kotlinJvm)
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
     alias(libs.plugins.gobleyCargo)
     alias(libs.plugins.gobleyUniffi)
     kotlin("plugin.atomicfu") version libs.versions.kotlin
 }
 
-dependencies {
+kotlin {
+    androidTarget {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_21)
+        }
+    }
 
+    jvm()
+}
+
+android {
+    namespace = "uniffi.hnefatafl"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
 }
 
 cargo {
@@ -18,5 +37,8 @@ cargo {
         // Build Rust library only for the host platform
         embedRustLibrary = (GobleyHost.current.rustTarget == rustTarget)
         jvmVariant = Variant.Release
+    }
+    builds.android {
+        // any config here?
     }
 }
