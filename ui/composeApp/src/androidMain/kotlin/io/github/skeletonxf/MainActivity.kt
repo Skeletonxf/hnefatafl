ad6f5382775268fb6300df3e9d65d60ad51afc12
Skeletonxf <skeletonxf@gmail.com>
2026-04-20 19:06:14 +0100

    Fix status bar colours being wrong

--- ui/composeApp/src/androidMain/kotlin/io/github/skeletonxf/MainActivity.kt
+++ ui/composeApp/src/androidMain/kotlin/io/github/skeletonxf/MainActivity.kt
@@ -1,7 +1,9 @@
 package io.github.skeletonxf
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
@@ -13,7 +15,9 @@ class MainActivity : ComponentActivity() {
     lateinit var appGlobalState: AppGlobalState
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT)
+        )
         super.onCreate(savedInstanceState)
 
         val appState = AppGlobalState.getInstance(applicationContext)
